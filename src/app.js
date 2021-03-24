fb406982fa578d8ad896bf4a4bf3163981be2f68
Dinesh G M <dinesh.gm@mrcooper.com>
2021-03-24 14:08:52 +0530

    In memory products

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
 const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
   res.send("All good!");
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,19 +1,60 @@
 const productsRouter = require('express').Router();
 
+const products = [{
+  id: 1,
+  name: "Samsung galaxy s20",
+  price: 60000,
+  sold: false
+}, {
+  id: 2,
+  name: "iPhone 12 Pro",
+  price: 110000,
+  sold: false
+}, {
+  id: 3,
+  name: "iPad Pro",
+  price: 71900,
+  sold: false
+}, {
+  id: 4,
+  name: "Apple Watch",
+  price: 39999,
+  sold: false
+}, {
+  id: 5,
+  name: "Airpods Pro",
+  price: 24999,
+  sold: false
+}]
+
 productsRouter.get('/', (req, res) => {
-  res.send("All good!");
+  return res.status(200).json(products);
 });
 
-productsRouter.post('/:productId', (req, res) => {
-  res.send("All good on POST!");
+productsRouter.post('/', (req, res) => {
+  products.push(req.body);
+  return res.status(200).json(products);
 });
 
 productsRouter.put('/:productId', (req, res) => {
-  res.send("All good on PUT!");
+  const productId = req.params.productId;
+  products.map((product) => {
+    if(product.id === parseInt(productId)) {
+      product.sold = true;
+    }
+    return product;
+  });
+  return res.status(200).json(products);
 });
 
 productsRouter.delete('/:productId', (req, res) => {
-  res.send("All good on DELETE!");
+  const productId = req.params.productId;
+  products.forEach((product,  i) => {
+    if(product.id === parseInt(productId)) {
+      products.splice(i,1);
+    }
+  })
+  return res.status(200).json(products);
 });
 
 module.exports = productsRouter;
